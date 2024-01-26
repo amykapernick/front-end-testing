56f980a246c7782351704132b69cadb86e39728b
Amy Kapernick <amy.kapernick@makerx.com.au>
2024-01-26 17:59:42 +0800

    cleaned up more packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import rehypeFigure from './src/utils/rehypeFigure';
 import externalLinks from "rehype-external-links";
-import captions from 'remark-captions'
 
 export default defineConfig({
-	site: 'https://blog.amyskapers.dev',
 	markdown: {
 		syntaxHighlight: 'prism',
 		rehypePlugins: [
@@ -14,19 +11,9 @@ export default defineConfig({
 			[rehypeFigure, { allImages: true, useTitle: true }],
 			'rehype-picture',
 		],
-		remarkPlugins: [
-			['remark-oembed', { syncWidget: true, asyncImg: true }],
-			[captions, {
-				external: {
-					code: 'Code:'
-				}
-			}],
-			'remark-hint',
-			'remark-squeeze-paragraphs',
-		],
 		extendDefaultPlugins: true,
 	},
-	integrations: [sitemap(), image({
+	integrations: [image({
 		serviceEntryPoint: '@astrojs/image/sharp'
 	})]
 });
