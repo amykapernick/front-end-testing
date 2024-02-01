1064908434cf6becd90295662f5088ff3c840aa8
Amy Kapernick <amy.kapernick@makerx.com.au>
2024-02-01 17:16:42 +0800

    fixed test

--- tests/playwright.spec.js
+++ tests/playwright.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test')
 
-test('Has "Home" in the title', async ({ page }) => {
+test(`Has "Amy's Blog" in the title`, async ({ page }) => {
 	await page.goto('/')
 
-	await expect(page).toHaveTitle(/Home/)
+	await expect(page).toHaveTitle(/Amy's Blog/)
 })
